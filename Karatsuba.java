1d587020b24ce7b8576dba529cfc79eb7b43b6f1
Aaron <ayamin1337@yahoo.com>
2014-06-28 22:22:32 -0400

    Update Karatsuba.java

--- Karatsuba.java
+++ Karatsuba.java
@@ -2,7 +2,7 @@
  * The Karatsuba algorithm is a multiplication algorithm developed by Anatolii Alexeevitch Karatsuba in 1960.
  * It operates in O(n^log2(3)) time (~ O(n^1.585)), with n being the number of digits of the numbers we are multiplying together.
  * Standard grade-school multiplication operates in O(n^2) time. Karatsuba's method is asymptotically much faster.
- * Normally, you can choose any base you want, but we will be using base 10 in this algorithm with m varying depending on the length of the algorithm.
+ * Normally, you can choose any base you want, but we will be using base 10 in this algorithm with m varying depending on the length of the input.
  * Specific details are included with an example in the comments before the actual method.
  * 
  * @author Ayamin
